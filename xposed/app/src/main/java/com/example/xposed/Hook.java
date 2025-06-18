810127229aedfb36ba6a555981a501653953a805
hongmengshikong <2152085933@qq.com>
2025-06-18 21:55:03 +0800

    xposed

--- xposed/app/src/main/java/com/example/xposed/Hook.java
+++ xposed/app/src/main/java/com/example/xposed/Hook.java
@@ -39,6 +39,60 @@ public class Hook implements IXposedHookLoadPackage {
                 }
             }
         });
+        XposedHelpers.findAndHookMethod("com.umeng.commonsdk.statistics.common.DataHelper", loadPackageParam.classLoader, "encrypt", "byte[]", "byte[]", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                byte[] plaintext = (byte[]) param.args[0];
+                byte[] key = (byte[]) param.args[1];
+
+                Log.d("kong","=== [AES 加密] 开始 ===");
+                Log.d("kong","[明文] " + safeToString(plaintext));
+                Log.d("kong","[密钥] " + safeToString(key));
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                byte[] ciphertext = (byte[]) param.getResult();
+                Log.d("kong","[密文 HEX] " + bytesToHex(ciphertext));
+                Log.d("kong","=== [AES 加密] 结束 ===");
+            }
+        });
+        XposedHelpers.findAndHookMethod("com.umeng.commonsdk.statistics.common.DataHelper", loadPackageParam.classLoader, "decrypt", "byte[]", "byte[]", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                byte[] ciphertext = (byte[]) param.args[0];
+                byte[] key = (byte[]) param.args[1];
+
+                Log.d("kong","=== [AES 解密] 开始 ===");
+                Log.d("kong","[密文 HEX] " + bytesToHex(ciphertext));
+                Log.d("kong","[密钥] " + safeToString(key));
+            }
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                byte[] plaintext = (byte[]) param.getResult();
+                Log.d("kong","[明文] " + safeToString(plaintext));
+                Log.d("kong","=== [AES 解密] 结束 ===");
+            }
+        });
+    }
+    private static String safeToString(byte[] bytes) {
+        if (bytes == null) return "null";
+        try {
+            return new String(bytes, "UTF-8");
+        } catch (Exception e) {
+            return "（无法转换为字符串）";
+        }
+    }
+    private static String bytesToHex(byte[] bytes) {
+        if (bytes == null) return "null";
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02X", b));
+        }
+        return sb.toString();
     }
 
 }
